0c92a7b05c85e81b724617134fe1117fe329bafa
dougdabaso <dougdabaso@gmail.com>
2018-11-22 00:24:33 -0200

    Add files via upload
    I have added added two functions that compute the Periodic Correlation statistics proposed by [1]. These functions are zetaMag(p,q,M,MyFFT) and PCStats(MySignalInTime,p,q,M)

--- initial_EMD_analysis_audio_clips.py
+++ initial_EMD_analysis_audio_clips.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import mode
+from numpy import *
 
 # For this proof of concept, we can use pyhht package (https://pyhht.readthedocs.io) which offers implementations of EMD
 #  and time-frequency (TF) transforms (which are the two approaches we are going to use a priori)
@@ -84,28 +85,69 @@ print('IMFs successfully built')
 
 # i) no speech
 MaskRelevantIMFsNoSpeech = relevant_IMFs(NoSpeechSignalIMFs,NoSpeechSignalDownsampled,thresh_range)
-MaskRelevantIMFsNoSpeech = np.transpose(np.array(MaskRelevantIMFsNoSpeech.astype('bool')))
+MaskRelevantIMFsNoSpeech = transpose(array(MaskRelevantIMFsNoSpeech.astype('bool')))
 NoSpeechSignalIMFs2 = NoSpeechSignalIMFs[MaskRelevantIMFsNoSpeech]
-DenoisedNoSpeechSignal = np.shape(np.sum(NoSpeechSignalIMFs2,0)) # Sum up the remaining IMFs to obtain denoised signal
+DenoisedNoSpeechSignal = sum(NoSpeechSignalIMFs2,0) # Sum up the remaining IMFs to obtain denoised signal
 plt.figure(0) # Plotting relevant IMFs + residue
 plot_imfs(NoSpeechSignalDownsampled, NoSpeechSignalIMFs2)
 
 
 # ii) speech with noise
 MaskRelevantIMFsSpeechWithNoise = relevant_IMFs(SpeechWithNoiseSignalIMFs,SpeechWithNoiseSignalDownsampled,thresh_range)
-MaskRelevantIMFsSpeechWithNoise = np.transpose(np.array(MaskRelevantIMFsSpeechWithNoise.astype('bool')))
+MaskRelevantIMFsSpeechWithNoise = transpose(array(MaskRelevantIMFsSpeechWithNoise.astype('bool')))
 SpeechWithNoiseSignalIMFs2 = SpeechWithNoiseSignalIMFs[MaskRelevantIMFsSpeechWithNoise]
-DenoisedSpeechWithNoiseSignal = np.shape(np.sum(SpeechWithNoiseSignalIMFs2,0)) # Sum up the remaining IMFs to obtain denoised signal
+DenoisedSpeechWithNoiseSignal = sum(SpeechWithNoiseSignalIMFs2,0) # Sum up the remaining IMFs to obtain denoised signal
 plt.figure(1) # Plotting relevant IMFs + residue
 plot_imfs(SpeechWithNoiseSignalDownsampled, SpeechWithNoiseSignalIMFs2)
 
 # iii) clean speech
 MaskRelevantIMFsCleanSpeech = relevant_IMFs(CleanSpeechSignalIMFs,CleanSpeechSignalDownsampled,thresh_range)
-MaskRelevantIMFsCleanSpeech = np.transpose(np.array(MaskRelevantIMFsCleanSpeech.astype('bool')))
+MaskRelevantIMFsCleanSpeech = transpose(array(MaskRelevantIMFsCleanSpeech.astype('bool')))
 CleanSpeechSignalIMFs2 = CleanSpeechSignalIMFs[MaskRelevantIMFsCleanSpeech]
-DenoisedCleanSpeechSignal = np.shape(np.sum(CleanSpeechSignalIMFs2,0)) # Sum up the remaining IMFs to obtain denoised signal
+DenoisedCleanSpeechSignal = sum(CleanSpeechSignalIMFs2,0) # Sum up the remaining IMFs to obtain denoised signal
 plt.figure(2) # Plotting relevant IMFs + residue
 plot_imfs(CleanSpeechSignalDownsampled, CleanSpeechSignalIMFs2)
+
+
+
+# Proof of concept: check if for the same p, q and M, the (in)coherence stats vary for the input signals
+
+p = 3
+q = 4
+M = 1000
+NoSpeechCoherentStat, NoSpeechIncoherentStat = PCStats(DenoisedNoSpeechSignal,p,q,M)
+SpeechWithNoiseCoherentStat, SpeechWithNoiseIncoherentStat = PCStats(DenoisedSpeechWithNoiseSignal,p,q,M)
+CleanSpeechCoherentStat, CleanSpeechIncoherentStat = PCStats(DenoisedCleanSpeechSignal,p,q,M)
+
+ProtoTypeFeatureVectorNoSpeech = array([NoSpeechCoherentStat,NoSpeechIncoherentStat])
+ProtoTypeFeatureVectorSpeechWithNoise = array([SpeechWithNoiseCoherentStat,SpeechWithNoiseIncoherentStat])
+ProtoTypeFeatureVectorCleanSpeech = array([CleanSpeechCoherentStat,CleanSpeechIncoherentStat])
+
+print(ProtoTypeFeatureVectorNoSpeech)
+print(ProtoTypeFeatureVectorSpeechWithNoise)
+print(ProtoTypeFeatureVectorCleanSpeech)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -117,6 +159,65 @@ plot_imfs(CleanSpeechSignalDownsampled, CleanSpeechSignalIMFs2)
 
 # Supporting functions -------------------------------------------------------------------------------------------------
 
+
+
+def PCStats(MySignalInTime,p,q,M):
+
+    # This function computes the statistics describing periodical correlations , as defined in [1] in eq. (6). As input,
+    #  it takes the signal in time, the value of p, q, and M.
+
+    MySignalInTime = MySignalInTime - mean(MySignalInTime) # Subtracting the mean for ACF calculation
+    # Computing the normalized autocorrelation function
+    AutoCorrFunc = correlate(MySignalInTime, MySignalInTime, mode='full')
+    NormAutoCorrFunc = AutoCorrFunc[int(ceil(AutoCorrFunc.size/2)):]/AutoCorrFunc[int(ceil(AutoCorrFunc.size/2))]
+
+    # For computations we wil be using the PSD, so the FFT of the autocorrelation function is computed
+    MyFFT = fft.fft(NormAutoCorrFunc)
+
+    # Computing coherence and incoherence statistics
+    NFFT=len(MyFFT) # Number of bins of the FFT
+    N=len(NormAutoCorrFunc) # Length of the time series used for analysis
+    tau = abs(q-p) # Required parameter for the calculations
+    L = (N-1-tau)/M # Required parameter for the calculations
+    print(L)
+    print(N)
+    print(tau)
+    CoherentStat=zetaMag(0,tau,M,MyFFT)
+    IncoherentStat=(1/(L+1))*zetaMag(p*M,p*M+tau,M,MyFFT)
+
+    return CoherentStat, IncoherentStat
+
+
+
+
+def zetaMag(p,q,M,MyFFT):
+    # This function computes the sample coherence, as defined in [1] in eq. (6). As input, it takes the current FFT
+    # of the desired time series (it could be normalized autocorrelation function), the value of p, q, and M.
+    Num = []
+    Den1 = []
+    Den2 = []
+
+    NFFT = len(MyFFT) # The number of bins used in FFT
+
+    for m in range(0, M):
+        Ind1FFT = p + m  # Indexes of FFT
+        Ind2FFT = q + m  # Indexes of FFT
+        # Taking into account the periodicity of the Discrete Fourier transform (check if it should be taken into consideration)
+        if (Ind1FFT > NFFT):
+            Ind1FFT = Ind1FFT - NFFT
+        if (Ind2FFT > NFFT):
+            Ind2FFT = Ind2FFT - NFFT
+        # Computing components of the numerator and the denominator of (6)
+        Num.append(abs(MyFFT[Ind1FFT] * conj(MyFFT[Ind2FFT])))
+        Den1.append(abs(MyFFT[Ind1FFT]) ** 2)
+        Den2.append(abs(MyFFT[Ind2FFT]) ** 2)
+
+    return (abs(sum(Num)) ** 2) / (sum(Den1) * sum(Den2))
+
+
+
+
+
 def relevant_IMFs(current_IMFs, current_signal, thresh_range):
     # This function executes the energy based approach proposed in [IMFICASSP14] for
     # computing the group of most relevant IMFs obtained from the EMD algorithm
@@ -126,6 +227,7 @@ def relevant_IMFs(current_IMFs, current_signal, thresh_range):
     # [IMFICASSP14] IEEE ICASSP 2014 - International Conference on Acoustic, Speech and
     # Signal Processing, "On selecting relevant intrinsic mode functions in em-
     # pirical mode decompositions: an energy-based approach",D. Baptista de Souza et al.
+
 
 
     number_of_modes_obtained = current_IMFs.shape[0]  # Total number of IMFs obtained from the decomposition
@@ -143,25 +245,25 @@ def relevant_IMFs(current_IMFs, current_signal, thresh_range):
         energy_vector.append(power_sig_imfs.sum())  # Storing the energy value of each IMF
         cov_vector = []  # To store the covariance between the i-th and the j-th IMFs
         for j in range(0, number_of_modes_obtained - 1):
-            if i != j: cov_matrix = np.cov(current_IMFs[i,], current_IMFs[j,])
+            if i != j: cov_matrix = cov(current_IMFs[i,], current_IMFs[j,])
             if i != j: cov_vector.append(cov_matrix[0, 1])  # covariance is the off-diag. terms
 
-        cov_matrix_residual = np.cov(current_IMFs[i,], current_IMFs[
+        cov_matrix_residual = cov(current_IMFs[i,], current_IMFs[
             number_of_modes_obtained - 1,])  # computing the covariance between the residual and the current IMF
-        epsilon_vector.append(np.sum(cov_vector) + cov_matrix_residual[0, 1])
+        epsilon_vector.append(sum(cov_vector) + cov_matrix_residual[0, 1])
 
     # 2) Computing the threshold using the energy-criterion
 
-    energy_x_p = np.sum((current_signal - np.mean(current_signal)) ** 2)
-    epsilon_final = np.sum(epsilon_vector)
+    energy_x_p = sum((current_signal - mean(current_signal)) ** 2)
+    epsilon_final = sum(epsilon_vector)
 
     # 3) For the collection of all possible thresh values, check the most frequent
     # modes, i.e., the most relevant IMFs
 
-    # outside_array_relevant_IMFs_vector = np.zeros((np.size(thresh_range),number_of_modes_obtained-2))
+    # outside_array_relevant_IMFs_vector = zeros((size(thresh_range),number_of_modes_obtained-2))
     outside_array_relevant_IMFs_vector = []
 
-    for i_thresh in range(0, np.size(thresh_range)):
+    for i_thresh in range(0, size(thresh_range)):
 
         thresh = thresh_range[i_thresh]
 
@@ -170,18 +272,18 @@ def relevant_IMFs(current_IMFs, current_signal, thresh_range):
         # Verifyign the condition for the i-th IMF
         for i in range(0, (number_of_modes_obtained-1)):
 
-            if (np.sqrt(energy_vector[i]) * ((np.sqrt(energy_x_p) * thresh) - np.sqrt(
+            if (sqrt(energy_vector[i]) * ((sqrt(energy_x_p) * thresh) - sqrt(
                 energy_vector[i]))) < epsilon_final: relevant_IMF_vector.append(1)
-            if (np.sqrt(energy_vector[i]) * ((np.sqrt(energy_x_p) * thresh) - np.sqrt(
+            if (sqrt(energy_vector[i]) * ((sqrt(energy_x_p) * thresh) - sqrt(
                 energy_vector[i]))) > epsilon_final: relevant_IMF_vector.append(0)
 
-        outside_array_relevant_IMFs_vector.append(np.transpose(relevant_IMF_vector))
+        outside_array_relevant_IMFs_vector.append(transpose(relevant_IMF_vector))
 
     # Computing the most frequent group of IMFs
     selected_mode = mode(outside_array_relevant_IMFs_vector)
 
     # Adding the residue to the decomposition
-    selected_relevant_IMFs = np.append(selected_mode.mode, 1)
+    selected_relevant_IMFs = append(selected_mode.mode, 1)
 
     return selected_relevant_IMFs
 
